bd75a177455dbc96016574fe31ec15f4fb24f478
Jannalepaaa <jannalepaaa@gmail.com>
2022-06-08 11:51:24 +0200

    master

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { HousingService } from './../../services/housing.service';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-list',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
-
-  constructor() { }
+  properties : any;
+  constructor(private housingService:HousingService) { }
 
   ngOnInit(): void {
+    this.housingService.getAllProperties().subscribe(
+        data=>{
+        this.properties=data;
+        console.log(data);
+      }, error => {
+        console.log(error);
+      }
+    )
   }
-
 }
